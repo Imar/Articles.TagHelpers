bd3020f470ff6b8a871c5bbfaa500a5817ccdb6c
Imar Spaanjaars <imar@spaanjaars.com>
2020-11-11 13:08:44 +0100

    Add project files.

---
+++ TagHelpers/SitePages.cs
@@ -0,0 +1,14 @@
+﻿namespace TagHelpers
+{
+  public static class SitePages
+  {
+    public class Demo
+    {
+      public const string Index = @"/Demo/Index";
+    }
+    public const string Error = @"/Error";
+    public const string Index = @"/Index";
+    public const string Privacy = @"/Privacy";
+  }
+}
+
